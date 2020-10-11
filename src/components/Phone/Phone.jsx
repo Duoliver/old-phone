c1df52c75798343d398e9d18bd3958e6307efb15
Honeysalad <oliveira.douglasedu@outlook.com>
2020-10-11 16:33:50 -0300

    added receiver

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Keyboard from './Keyboard'
 
 import './phone.scss'
+import Receiver from './Receiver/Receiver'
 import Screen from './Screen/Screen'
 
 export default function Phone() {
   return (
     <div className="shell">
-      <div className="earthing-container">
-
+      <div className="receiver-container">
+        <Receiver/>
       </div>
       <div className="screen-container">
         <div className="screen-border">
---
+++ src/components/Phone/Receiver/Receiver.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './receiver.scss' 
+
+export default function Receiver() {
+  return (
+    <div className="receiver">
+      <div />
+    </div>
+  )
+}
