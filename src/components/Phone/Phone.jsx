719d1c8a432d7007fad36c2abfdcece8f99932df
Honeysalad <oliveira.douglasedu@outlook.com>
2020-10-10 21:17:43 -0300

    basic phone layout wireframe

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -5,7 +5,15 @@ import './phone.scss'
 export default function Phone() {
   return (
     <div className="shell">
-      
+      <div className="earthing">
+
+      </div>
+      <div className="screen">
+
+      </div>
+      <div className="keyboard">
+
+      </div>
     </div>
   )
 }
